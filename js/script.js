777bb8e0864e992253be4ff7afd2e5b804832c7a
AlessandroPagliuca <alessandro2ap3@gmail.com>
2023-03-14 19:05:54 +0100

    Upgrade increase and decrease index of images

--- js/script.js
+++ js/script.js
@@ -14,12 +14,6 @@ MILESTONE 3
 Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, che quindi verrà visualizzata al posto della precedente.
 */
 
-/*
- 
-`<img src="./img/01.webp" alt="" class="active">`
- 
-*/
-
 // create array with img
 const images = ['./img/01.webp', './img/02.webp', './img/03.webp', './img/04.webp', './img/05.webp'];
 console.log(images);
@@ -29,45 +23,65 @@ let slideImg = document.querySelector('img#slide');
 slideImg.src = images[currentImg];
 console.log(slideImg.src)
 
-//button top 
+//create variable and function for btn top
 const c92BtnTop = document.querySelector('.c92BtnTop');
 c92BtnTop.addEventListener('click', c92MoveTop);
 
+//start function for btn top
 function c92MoveTop(){
     if(currentImg <= 0){
         currentImg = images.length;
         
     }
-    
+    //decrease index of images(array) for switch image
     currentImg--;
     let slideImg = document.querySelector('img#slide');
     slideImg.src = images[currentImg];
 
+    //remove class c92mainThumbnails from whoever owns it and add opacity to the selected thumbs
+    let thumbs = document.querySelectorAll('.c92thumbnails');
+    let mainthumb = document.querySelector('.c92mainThumbnails');
+    mainthumb.classList.remove('c92mainThumbnails');
+    thumbs[currentImg].classList += ' c92mainThumbnails';
 }
 
-//button bot
+
+//create variable and function for btn bot
 const c92BtnBot = document.querySelector('.c92BtnBot');
 c92BtnBot.addEventListener('click', c92MoveBot);
 console.log('bot');
 
+//start function for btn bot
 function c92MoveBot(){
     if(currentImg >= images.length - 1){
         currentImg = - 1;
         
     }
-        
+    
+    //increase index of images(array) for switch image
     currentImg++;
     let slideImg = document.querySelector('img#slide');
     slideImg.src = images[currentImg];
-   
+
+    //remove class c92mainThumbnails from whoever owns it and add opacity to the selected thumbs
+    let thumbs = document.querySelectorAll('.c92thumbnails');
+    let mainthumb = document.querySelector('.c92mainThumbnails');
+    mainthumb.classList.remove('c92mainThumbnails');
+    thumbs[currentImg].classList += ' c92mainThumbnails';
+
 }
 
 
+// generate right thumbs
 
 for (let i = 0; i < images.length; i++) {
     let slideImgRight = document.createElement('div');
     slideImgRight.innerHTML = `<img src= "${images[i]}"></img>`;
+    if(i === 0)
+    slideImgRight.classList += ' c92mainThumbnails';
+    slideImgRight.classList += ' c92thumbnails';
     const c92BoxRight = document.querySelector('.c92BoxRight');
     c92BoxRight.appendChild(slideImgRight);
     console.log(slideImgRight);
 }  
+
